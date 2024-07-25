f181650ddd70e39eceeab568ad0f1be35c9fe989
Maki <maki@hotmilk.space>
2024-07-25 08:56:40 -0400

    Cleanup

--- make-firefox-zip.sh
+++ make-firefox-zip.sh
@@ -1,5 +1,13 @@
-mv manifest.json manifest-vivaldi.json
+#!/bin/bash
+
+mv manifest.json manifest-chromium.json
 mv manifest-firefox.json manifest.json
-zip -r -FS ./new-tab.zip * --exclude '*.git*' --exclude 'manifest-vivaldi.json' --exclude '*node_modules*'
+
+zip -r -FS ./new-tab.zip * \
+	--exclude '*.git*' --exclude '*.*' \
+	--exclude 'manifest-chromium.json' \
+	--exclude '*.zip' --exclude '*.lock' \
+	--exclude '*node_modules*'
+
 mv manifest.json manifest-firefox.json
-mv manifest-vivaldi.json manifest.json
+mv manifest-chromium.json manifest.json
