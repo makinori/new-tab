5fc92aa4a77b19524fa9454078f2756a3ffdc918
Maki <maki@hotmilk.space>
2024-07-25 09:00:23 -0400

    Reorganize and update make firefox zip

--- make-firefox-zip.sh
+++ make-firefox-zip.sh
@@ -4,10 +4,9 @@ mv manifest.json manifest-chromium.json
 mv manifest-firefox.json manifest.json
 
 zip -r -FS ./new-tab.zip * \
-	--exclude '*.git*' --exclude '*.*' \
-	--exclude 'manifest-chromium.json' \
-	--exclude '*.zip' --exclude '*.lock' \
-	--exclude '*node_modules*'
+	--exclude '*.lock' --exclude '*.zip' --exclude '*.sh' --exclude '*.ts' \
+	--exclude '*.git*' --exclude 'manifest-chromium.json' \
+	--exclude '*node_modules*' --exclude 'unused/*'
 
 mv manifest.json manifest-firefox.json
 mv manifest-chromium.json manifest.json
